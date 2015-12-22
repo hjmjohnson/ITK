2db5b39d5bfe1e3724217f41ece08818517f0d43
Zach Williamson <zachary-williamson@uiowa.edu>
2015-12-22 14:07:55 -0600

    WIP: Resolve more test failures

--- Modules/Filtering/LabelMap/include/itkLabelMapExplicit.h
+++ Modules/Filtering/LabelMap/include/itkLabelMapExplicit.h
@@ -2,20 +2,25 @@
 #define itkLabelMapExplicit_h
 
 #include "itkLabelObject.h"
-#include "itkShapeLabelObject.h"
-#include "itkStatisticsLabelObject.h"
 
 extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::LabelObject<unsigned char, 3u> >;
 extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::LabelObject<signed char, 2u> >;
-extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::ShapeLabelObject<unsigned char, 2u> >;
-extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::StatisticsLabelObject<unsigned long, 2u> >;
 extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::LabelObject<unsigned long, 2u> >;
+extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::LabelObject<unsigned long, 3u> >;
+extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::LabelObject<unsigned char, 2u> >;
+
+#ifndef ITKShapeLabelObjectIncludes
+#include "itkShapeLabelObject.h"
+#include "itkStatisticsLabelObject.h"
+
+extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::ShapeLabelObject<unsigned char, 2u> >;
 extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::ShapeLabelObject<unsigned char, 3u> >;
 extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::ShapeLabelObject<unsigned long, 2u> >;
-extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::LabelObject<unsigned long, 3u> >;
+
+extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::StatisticsLabelObject<unsigned long, 2u> >;
 extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::StatisticsLabelObject<unsigned char, 3u> >;
-extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::LabelObject<unsigned char, 2u> >;
 extern template class ITKLabelMap_EXPORT itk::LabelMap<itk::StatisticsLabelObject<unsigned char, 2u> >;
+#endif //ITKShapeLabelObjectIncludes
 
 #ifndef ITKAttributeLabelObjectIncludes
 #include "itkAttributeLabelObject.h"
